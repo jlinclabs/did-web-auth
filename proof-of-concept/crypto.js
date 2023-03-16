49eeb77e931d1df5a726b7677d525e7f970eee51
Jared Grippe <jared@deadlyicon.com>
2023-03-16 08:56:46 -0700

    WIP

--- proof-of-concept/crypto.js
+++ proof-of-concept/crypto.js
@@ -2,6 +2,7 @@ import { promisify } from 'util'
 import crypto from 'crypto'
 import ed25519 from 'ed25519'
 import * as jose from 'jose'
+import forge from 'node-forge'
 import { base64url } from 'multiformats/bases/base64'
 import { base58btc } from 'multiformats/bases/base58'
 
@@ -32,11 +33,58 @@ export function keyToBuffer(string){
 }
 
 export async function generateSigningKeyPair(seed){
-  const generateKeyPair = promisify(crypto.generateKeyPair).bind(null, 'ed25519')
+  // node's crypto.generateKeyPair has no way to provide a seed
+  // const generateKeyPair = promisify(crypto.generateKeyPair).bind(null, 'ed25519')
+  // const { publicKey, privateKey } = await generateKeyPair()
+
   // WE NEED A DETERMINISTIC WAY TO MAKE THESE!?!?!
-  const { publicKey, privateKey } = await generateKeyPair()
+
+  if (seed){
+    const seedBuffer = Buffer.alloc(32)
+    Buffer.from(seed).copy(seedBuffer)
+    seed = seedBuffer
+  }
+
+  // const seed = forge.random.getBytesSync(32);
+  let { publicKey, privateKey } = forge.pki.ed25519
+    .generateKeyPair({ seed })
+
+  publicKey = publicKeyFromBuffer(createEd25519PublicKeySpkiBuffer(publicKey))
+  privateKey = privateKeyFromBuffer(createEd25519PrivateKeyPkcs8Buffer(privateKey))
+
   return { publicKey, privateKey }
 }
+
+// Magic ChatGPT wrote for me :D
+function createEd25519PublicKeySpkiBuffer(publicKeyBuffer) {
+  const prefix = Buffer.from('302a300506032b6570032100', 'hex');
+  return Buffer.concat([prefix, publicKeyBuffer]);
+}
+function createEd25519PrivateKeyPkcs8Buffer(privateKeyBuffer) {
+  const prefix = Buffer.from('302e020100300506032b657004220420', 'hex');
+  return Buffer.concat([prefix, privateKeyBuffer]);
+}
+function ed25519PublicJwkToSpkiBuffer(jwk) {
+  const prefix = Buffer.from('302a300506032b6570032100', 'hex');
+  const publicKeyBuffer = Buffer.from(jwk.x, 'base64');
+  return Buffer.concat([prefix, publicKeyBuffer]);
+}
+function ed25519PrivateJwkToPkcs8Buffer(jwk) {
+  const prefix = Buffer.from('302e020100300506032b657004220420', 'hex');
+  const privateKeyBuffer = Buffer.from(jwk.d, 'base64');
+  return Buffer.concat([prefix, privateKeyBuffer]);
+}
+function privateKeyJwkToPublicKeyJwk(privateKeyJwk) {
+  const publicKeyJwk = {
+    kty: privateKeyJwk.kty,
+    crv: privateKeyJwk.crv,
+    x: privateKeyJwk.x,
+    alg: privateKeyJwk.alg,
+    ext: privateKeyJwk.ext,
+  };
+  return publicKeyJwk;
+}
+
 
 export function publicKeyToBuffer(publicKey){
   return publicKey.export({
@@ -44,7 +92,6 @@ export function publicKeyToBuffer(publicKey){
     format: 'der',
   })
 }
-
 export function privateKeyToBuffer(privateKey){
   return privateKey.export({
     type: 'pkcs8',
@@ -75,18 +122,28 @@ export function privateKeyToJKW(privateKey){
   return privateKey.export({ format: 'jwk' })
 }
 
-
 export function publicKeyFromJKW(publicKeyJWK){
-  // return publicKey.export({ format: 'jwk' })
+  return crypto.createPublicKey({
+    key: ed25519PublicJwkToSpkiBuffer(publicKeyJWK),
+    format: 'der',
+    type: 'spki',
+  });
 }
 
 export function privateKeyFromJKW(privateKeyJWK){
-  // return privateKey.export({ format: 'jwk' })
+  return crypto.createPrivateKey({
+    key: ed25519PrivateJwkToPkcs8Buffer(privateKeyJWK),
+    format: 'der',
+    type: 'pkcs8',
+  })
 }
+
+
 
 async function publicKeyToKeyObject(publicKey){
   return crypto.KeyObject.create(keyToBuffer(publicKey))
 }
+
 async function privateKeyToKeyObject(privateKey){
   console.log({ privateKey })
   const x = {
@@ -151,10 +208,10 @@ export async function createJWS({ payload, signers }){
     proto
       // .addSignature(privateKey)
       .addSignature(
-        await privateKeyToKeyObject(privateKey)
-        // privateKey
+        // await privateKeyToKeyObject(privateKey)
+        privateKey
       )
-      .setProtectedHeader({ alg: 'ed25519' })
+      .setProtectedHeader({ alg: 'EdDSA' })
   }
     // .addSignature(ecPrivateKey)
     // .setProtectedHeader({ alg: 'ES256' })
--- proof-of-concept/test/crypto.test.js
+++ proof-of-concept/test/crypto.test.js
@@ -2,6 +2,9 @@ import test from 'brittle'
 import {
   generateSigningKeyPair,
   publicKeyToJKW,
+  privateKeyToJKW,
+  publicKeyFromJKW,
+  privateKeyFromJKW,
   keyBufferToString,
   keyToBuffer,
   createJWS,
@@ -13,50 +16,77 @@ test('generate keys from seed', async t => {
     publicKeyToJKW(kp1.publicKey),
     {
       crv: 'Ed25519',
-      x: '9xHbcUKsllob4fu75FedlPN_FpJeGabXauQfCJo6gDo',
+      x: 'Odqt3JEB83JgwD1oGzv9lavRV0XxI4231BtzU5X1t4o',
       kty: 'OKP',
     }
   )
-  // t.alike(
-  //   await generateSigningKeyPair('seed two'),
-  //   {
-  //     publicKey: 'z56NkB1NtdVvq4CiCSNz46EZH23nMuRjZJy7Y8L8M4mTE',
-  //     privateKey: 'z4jr374ChpDMhgiAMPGeBNDf6ntYJY6eaWyqDRCDf6RMLLzsKrYw21SJRA6SgaTdzjVKEmBy3XnW7SHd21YH8rXBe',
-  //   }
-  // )
-  // t.unlike(
-  //   await generateSigningKeyPair(),
-  //   {
-  //     publicKey: 'z56NkB1NtdVvq4CiCSNz46EZH23nMuRjZJy7Y8L8M4mTE',
-  //     privateKey: 'z4jr374ChpDMhgiAMPGeBNDf6ntYJY6eaWyqDRCDf6RMLLzsKrYw21SJRA6SgaTdzjVKEmBy3XnW7SHd21YH8rXBe',
-  //   }
-  // )
+  t.alike(
+    privateKeyToJKW(kp1.privateKey),
+    {
+      crv: 'Ed25519',
+      d: 'c2VlZCBvbmUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+      x: 'Odqt3JEB83JgwD1oGzv9lavRV0XxI4231BtzU5X1t4o',
+      kty: 'OKP',
+    }
+  )
+  const kp2 = await generateSigningKeyPair('seed two')
+  t.alike(
+    publicKeyToJKW(kp2.publicKey),
+    {
+      crv: 'Ed25519',
+      x: '19EcNkJdcqM0_K8qY0nWnzQvkYtB4T7WB0kC4aRUHtE',
+      kty: 'OKP',
+    }
+  )
+  t.alike(
+    privateKeyToJKW(kp2.privateKey),
+    {
+      crv: 'Ed25519',
+      d: 'c2VlZCB0d28AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+      x: '19EcNkJdcqM0_K8qY0nWnzQvkYtB4T7WB0kC4aRUHtE',
+      kty: 'OKP',
+    }
+  )
 })
 
-// test('serialize keys', async t => {
-//   const kp = await generateSigningKeyPair()
-//   t.is(
-//     kp.publicKey,
-//     keyBufferToString(keyToBuffer(kp.publicKey))
-//   )
-//   t.is(
-//     kp.privateKey,
-//     keyBufferToString(keyToBuffer(kp.privateKey))
-//   )
-// })
+test.solo('serialize keys', async t => {
+  const kp = await generateSigningKeyPair()
+  t.alike(
+    kp.publicKey,
+    publicKeyFromJKW(publicKeyToJKW(kp.publicKey))
+  )
+  t.alike(
+    kp.privateKey,
+    privateKeyFromJKW(privateKeyToJKW(kp.privateKey))
+  )
+})
 
-// test('JWS', async t => {
-//   const kp1 = await generateSigningKeyPair()
-//   const kp2 = await generateSigningKeyPair()
-//   const jws = await createJWS({
-//     payload: { stuff: true },
-//     signers: [
-//       kp1.privateKey,
-//       kp2.privateKey,
-//     ]
-//   })
+test('JWS', async t => {
+  const kp1 = await generateSigningKeyPair('JWS Test KP 1')
+  const kp2 = await generateSigningKeyPair('JWS Test KP 2')
 
-// })
+  const jws = await createJWS({
+    payload: { stuff: true },
+    signers: [
+      kp1.privateKey,
+      kp2.privateKey,
+    ]
+  })
+
+  t.alike(jws, {
+    payload: 'eyJzdHVmZiI6dHJ1ZX0',
+    signatures: [
+      {
+        signature: 'N0N-LQM55FGs6yvBD1lu2efvbM9MgfnC9J0FRfAfjdQukIfcpMehxlPb4cFMzR4Co2b_b_cPCWigu-_IpIDaDA',
+        protected: 'eyJhbGciOiJFZERTQSJ9',
+      },
+      {
+        signature: 'f7Oh2onkcNrt_0YDZTSKIOYuwTs4NZcPee2IqPOTE-BmWe2IprmOLWzlzi4t7e2cFotBOcU_Ribgj6olRDnOAQ',
+        protected: 'eyJhbGciOiJFZERTQSJ9',
+      }
+    ],
+  })
+})
 
 // // test('JWK', async t => {
 // //   const kp1 = await generateSigningKeyPair('seed one')
