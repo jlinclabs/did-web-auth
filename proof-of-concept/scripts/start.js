57fafe8e1d2348999223377a9c07ef983fcee4d0
Jared Grippe <jared@deadlyicon.com>
2023-03-31 13:14:32 -0700

    scripts/starts.js now looks at HOST and PORT env vars

--- proof-of-concept/scripts/start.js
+++ proof-of-concept/scripts/start.js
@@ -4,14 +4,32 @@ import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import app from '../app.js'
 
-const argv = yargs(hideBin(process.argv))
+const { host, port } = yargs(hideBin(process.argv))
   .usage('Usage: $0 --port 3001 --host app.test')
-  .demandOption(['port','host'])
-  .argv
+  .option('host', {
+    alias: 'h',
+    type: 'number',
+    description: 'host',
+    default: process.env.HOST,
+  })
+  .option('port', {
+    alias: 'p',
+    type: 'number',
+    description: 'port',
+    default: process.env.PORT,
+  })
+  .parse()
 
+const bail = message => {
+  console.error(message)
+  process.exit(1)
+}
+
+if (!host || typeof host !== 'string') bail(`host is required`)
+if (!port || typeof port !== 'string') bail(`port is required`)
 app
-  .set('port', argv.port)
-  .set('host', argv.host)
+  .set('port', port)
+  .set('host', host)
   .start()
   .catch(error => {
     console.error(error)
